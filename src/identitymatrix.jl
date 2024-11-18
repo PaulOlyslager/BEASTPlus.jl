cf456591e4b36a66b26269ee3f083540fc8add2b
PaulOlyslager <Paul.olyslager@ugent.be>
2024-11-18 15:27:20 +0100

    update

--- src/identitymatrix.jl
+++ src/identitymatrix.jl
@@ -1,6 +1,6 @@
 struct IdentityMatrix <: BEAST.AbstractOperator end
 export IdentityMatrix
-function assemble(op::IdentityMatrix, test_functions, trial_functions;
+function BEAST.assemble(op::IdentityMatrix, test_functions, trial_functions;
     storage_policy = Val{:bandedstorage},
     quadstrat=nothing) 
     return sparse(I, length(test_functions), length(trial_functions))
