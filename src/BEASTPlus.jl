a67aa853f8f3c1d4e7b64b402930a41d8badbb29
PaulOlyslager <Paul.olyslager@ugent.be>
2025-01-16 15:07:38 +0100

    added some kernels

--- src/BEASTPlus.jl
+++ src/BEASTPlus.jl
@@ -4,4 +4,5 @@ using SparseArrays
 # Write your package code here.
 include("quadstrat.jl")
 include("identitymatrix.jl")
+include("kernels.jl")
 end
---
+++ src/kernels.jl
@@ -0,0 +1,80 @@
+using BEAST
+
+struct HH3DGradGreen2{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+function (op::HH3DGradGreen2)(x,y)
+    gamma = op.gamma
+
+    r = cartesian(x) - cartesian(y)
+    R = norm(r)
+    gradgreen = -((expm1(-gamma*R)*(1+gamma*R)+gamma*R)/(4*pi*R^3))*r
+    return gradgreen
+end
+struct HH3DGreen2{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+function (op::HH3DGreen2)(x,y)
+    gamma = op.gamma
+
+    r = cartesian(x) - cartesian(y)
+    R = norm(r)
+    iR = 1/R
+    green = expm1(-gamma*R)*(i4pi*iR)
+    green
+end
+
+struct HH3DGradGreenFF{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+
+struct HH3DGreenFF{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+struct HH3DGradGreenFF2{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+
+struct HH3DGreenFF2{T} <: BEAST.Kernel{T}
+    gamma::T
+end
+
+
+# function (op::HH3DGreenFF)(x,y)
+#     gamma = op.gamma
+
+#     r = cartesian(x) - cartesian(y)
+#     R = norm(r)
+#     iR = 1/R
+#     green = exp(-gamma*R)*(i4pi*iR)
+#     green
+# end
+function (op::HH3DGreenFF)(x::Union{SVector,Vector},y)
+    gamma = op.gamma
+    x = normalize(x)
+    green = exp(gamma*(x⋅y))*(i4pi)
+    green
+end
+function (op::HH3DGradGreenFF)(x::Union{SVector,Vector},y)
+    gamma = op.gamma
+    x = normalize(x)
+    green = exp(gamma*(x⋅y))*(i4pi)
+    gradgreen = -gamma  * green * x
+    gradgreen
+end
+
+function (op::HH3DGreenFF2)(x::Union{SVector,Vector},y)
+    gamma = op.gamma
+    x = normalize(x)
+    green = expm1(gamma*(x⋅y))*(i4pi)
+    green
+end
+function (op::HH3DGradGreenFF2)(x::Union{SVector,Vector},y)
+    gamma = op.gamma
+    x = normalize(x)
+    green = expm1(gamma*(x⋅y))*(i4pi)
+    gradgreen = -gamma  * green * x
+    gradgreen
+end
+export HH3DGradGreen2, HH3DGreen2, HH3DGradGreenFF, HH3DGreenFF, HH3DGradGreenFF2, HH3DGreenFF2
+#### exp(-ikr)/r omitted
