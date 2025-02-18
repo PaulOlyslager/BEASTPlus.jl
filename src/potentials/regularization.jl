49dfa346c66846219d4adfa05023b9d40605f72c
PaulOlyslager <Paul.olyslager@ugent.be>
2025-02-18 14:46:38 +0100

    update

--- src/potentials/regularization.jl
+++ src/potentials/regularization.jl
@@ -10,18 +10,18 @@ function regularize(a::LinearCombinationOfPotentials)
 end
 
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGradGreen, <: Cross,W}) where {W}
-    return -regularize(BEAST.PotentialOperator{2}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),strace(pot.bfunc))) +
-        regularize(BEAST.PotentialOperator{3}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),curl(pot.bfunc)))
+    return -regularize(BEAST.PotentialOperator{2}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),b->strace(pot.bfunc(b)))) +
+        regularize(BEAST.PotentialOperator{3}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),b->curl(pot.bfunc(b))))
 end
 
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGreen, <: Times,W}) where {W}
-    return -regularize(BEAST.PotentialOperator{2}(HH3DInt1,Times(),ntrace(pot.bfunc))) +
-        regularize(BEAST.PotentialOperator{3}(HH3DInt1,Times(),divergence(pot.bfunc))) +
-        regularize(BEAST.PotentialOperator{2}(HH3DInt1,Cross(),strace(pot.bfunc))) -
-        regularize(BEAST.PotentialOperator{3}(HH3DInt1,Cross(),curl(pot.bfunc)))
+    return -regularize(BEAST.PotentialOperator{2}(HH3DInt1,Times(),b->ntrace(pot.bfunc(b)))) +
+        regularize(BEAST.PotentialOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
+        regularize(BEAST.PotentialOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b)))) -
+        regularize(BEAST.PotentialOperator{3}(HH3DInt1,Cross(),b->curl(pot.bfunc(b))))
 end
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<: HH3DGradDivGreen, <: Times,W}) where {W}
     @warn "assumed basis function is div conforming and normal zero on boundary"
     @warn "assumed basis functio is of order 0"
-    return -regularize(BEAST.PotentialOperator{2}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),ntimestrace(pot.bfunc))) 
+    return -regularize(BEAST.PotentialOperator{2}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),b->ntimestrace(pot.bfunc(b)))) 
 end
