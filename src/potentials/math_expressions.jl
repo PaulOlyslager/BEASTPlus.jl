7e41db423cad2e2bf59fbe1233150fe766bdbc28
PaulOlyslager <Paul.olyslager@ugent.be>
2025-02-19 13:03:21 +0100

    update

--- src/potentials/math_expressions.jl
+++ src/potentials/math_expressions.jl
@@ -6,13 +6,13 @@ struct RealDot <: _TupleFunction end
 
 function BEAST.potential(op::BEAST.PotentialIntegralOperator{D,U, <: _TupleFunction,W}, points, coeffs, basis; 
     type=SVector{3,ComplexF64},
-	quadstrat=nothing) where {D,U,W}
-    if quadstrat === nothing
-        return potential(BEAST.PotentialIntegralOperatorKern(op.kernel,operator(op.op2)),points,coeffs,op.bfunc(basis);type)
-    else
+	quadstrat=BEAST.defaultquadstrat(op,basis)) where {D,U,W}
+    # if quadstrat === nothing
+    #     return potential(BEAST.PotentialIntegralOperatorKern(op.kernel,operator(op.op2)),points,coeffs,op.bfunc(basis);type)
+    # else
         return potential(BEAST.PotentialIntegralOperatorKern(op.kernel,operator(op.op2)),points,coeffs,op.bfunc(basis);type,quadstrat)
 
-    end
+    # end
 end
 operator(op::Times) = *
 operator(op::Cross) = ×
