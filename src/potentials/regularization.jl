61232fa7327b8be888be255be67895fa18b9f505
PaulOlyslager <Paul.olyslager@ugent.be>
2025-02-19 12:06:33 +0100

    update

--- src/potentials/regularization.jl
+++ src/potentials/regularization.jl
@@ -14,11 +14,17 @@ function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGradGreen
         regularize(BEAST.PotentialIntegralOperator{3}(BEAST.HH3DGreen(pot.kernel.gamma),Times(),b->curl(pot.bfunc(b))))
 end
 
+# function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGreen, <: Times,W}) where {W}
+#     return (-1)*regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Times(),b->ntrace(pot.bfunc(b)))) +
+#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
+#         regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b)))) -
+#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Cross(),b->curl(pot.bfunc(b))))
+# end
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGreen, <: Times,W}) where {W}
-    return (-1)*regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Times(),b->ntrace(pot.bfunc(b)))) +
-        regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
-        regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b)))) -
-        regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Cross(),b->curl(pot.bfunc(b))))
+    @warn "assumed basis function is normal zero on boundary"
+    @warn "assumes curl is zero of basis function"
+    return regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
+        regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b))))
 end
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<: HH3DGradDivGreen, <: Times,W}) where {W}
     @warn "assumed basis function is div conforming and normal zero on boundary"
