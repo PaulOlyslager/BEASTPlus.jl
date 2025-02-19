fffd4fc80775b21d1aac0fec1bbcb19e7dcf03d5
PaulOlyslager <Paul.olyslager@ugent.be>
2025-02-19 13:18:55 +0100

    update

--- src/potentials/regularization.jl
+++ src/potentials/regularization.jl
@@ -15,16 +15,16 @@ function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGradGreen
 end
 
 # function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGreen, <: Times,W}) where {W}
-#     return (-1)*regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Times(),b->ntrace(pot.bfunc(b)))) +
-#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
-#         regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b)))) -
-#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Cross(),b->curl(pot.bfunc(b))))
+#     return (-1)*regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1(pot.kernel.gamma),Times(),b->ntrace(pot.bfunc(b)))) +
+#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1(pot.kernel.gamma),Times(),b->divergence(pot.bfunc(b)))) +
+#         regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1(pot.kernel.gamma),Cross(),b->strace(pot.bfunc(b)))) -
+#         regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1(pot.kernel.gamma),Cross(),b->curl(pot.bfunc(b))))
 # end
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<:BEAST.HH3DGreen, <: Times,W}) where {W}
     @warn "assumed basis function is normal zero on boundary"
     @warn "assumes curl is zero of basis function"
-    return regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1,Times(),b->divergence(pot.bfunc(b)))) +
-        regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1,Cross(),b->strace(pot.bfunc(b))))
+    return regularize(BEAST.PotentialIntegralOperator{3}(HH3DInt1(pot.kernel.gamma),Times(),b->divergence(pot.bfunc(b)))) +
+        regularize(BEAST.PotentialIntegralOperator{2}(HH3DInt1(pot.kernel.gamma),Cross(),b->strace(pot.bfunc(b))))
 end
 function regularize(pot::BEAST.PotentialIntegralOperator{3,<: HH3DGradDivGreen, <: Times,W}) where {W}
     @warn "assumed basis function is div conforming and normal zero on boundary"
